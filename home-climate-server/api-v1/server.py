37b2ac3e3549e01d63e32882329b4bad9d4eca18
Nabz786 <nabvali98@gmail.com>
2022-02-12 19:21:25 -0500

    Modified historical endpoint to call averages util function

--- home-climate-server/api-v1/server.py
+++ home-climate-server/api-v1/server.py
@@ -1,9 +1,17 @@
 from typing import Optional
 from fastapi import FastAPI
+from climate_utils import *
+import json
 
 app = FastAPI()
 
+'''
+Returns the average temp & humidity across the following ranges
+3hrs, 6hrs, 12hrs, 24hrs
+'''
+@app.get("/historical/")
+def full_range():
+    climate_averages = get_averages()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    return json.dumps(climate_averages)
+
